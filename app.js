9a33328b909b0656c2567cebca03c237719f259f
sayiO <sayionias@gmail.com>
2021-07-10 22:46:33 +0200

    modified the javascript file, added the cross functionality on the list items

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myListItems = document.getElementsByTagName("LI");
+var myListItems = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myListItems.length; i++) {
     var span = document.createElement("SPAN");
@@ -6,4 +6,30 @@ for (i = 0; i < myListItems.length; i++) {
     span.className = "close";
     span.appendChild(txt);
     myListItems[i].appendChild(span);
+}
+
+function newItemsAdded() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myList")
+    value;
+    var t = document.createTextNode(inputValue);
+    list.append(t);
+    if (inputValue === " ") {
+        alert("enter item");
+    } else {
+        document.getElementById("myul").appendChild(li);
+    }
+    document.getElementById("myList").value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            dic.style.display = "none";
+
+        }
+    }
 }
